3f726882ee0a044b5d480eb357461e6008ec56c2
NGUYENTHIMAIANH <maianhntsci94@gmail.com>
2018-09-16 10:45:15 +0700

    Finish topic 01- Check enviroment

---
+++ src/selenium_api/Topic_03_WebBrowser_WebElement.java
@@ -0,0 +1,19 @@
+package selenium_api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Topic_03_WebBrowser_WebElement {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
